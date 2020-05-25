e1c3f95577c426f3caa3157ade4c524163921e03
Denis Karpov <KarpovDL@hotmail.com>
2020-05-25 14:03:51 +0300

    Change app:jfrog-artifactoty-oss.

--- app/jfrog/artifactory-oss/Dockerfile/win/update.exec.ps1
+++ app/jfrog/artifactory-oss/Dockerfile/win/update.exec.ps1
@@ -7,9 +7,10 @@
 #$2 - version application prev
 #$3 - version application next
 #$4 - app port
-#$5 - sub path for data folder
-#$6 - docker id
+#$5 - app port ui
+#$6 - sub path for data folder
+#$7 - docker id
 
-#.\update.ps1 {APP_NAME} {VERSION_PREV} {VERSION_NEXT} {APP_PORT} {DATA_DIR} {DockerID}
+#.\update.ps1 {APP_NAME} {VERSION_PREV} {VERSION_NEXT} {APP_PORT} {APP_PORT_UI} {DATA_DIR} {DockerID}
 
-powershell -noexit "& "".\update.ps1 jfrog-artifactory-oss 7.4.3 7.4.3 8006 jfrog/artifactory-oss karpovdl"""
+powershell -noexit "& "".\update.ps1 jfrog-artifactory-oss 7.4.3 7.4.3 8081 8082 jfrog/artifactory-oss karpovdl"""
--- app/jfrog/artifactory-oss/Dockerfile/win/update.ps1
+++ app/jfrog/artifactory-oss/Dockerfile/win/update.ps1
@@ -8,15 +8,17 @@
 #$args[1] - version prev
 #$args[2] - version next
 #$args[3] - app port
-#$args[4] - sub path for data folder
-#$args[5] - docker id
+#$args[4] - app port ui
+#$args[5] - sub path for data folder
+#$args[6] - docker id
 
 $appName    = $args[0]
 $appVerPrev = $args[1]
 $appVerNext = $args[2]
 $appPort    = $args[3]
-$appDataDir = $args[4]
-$uid        = $args[5]
+$appPortUi  = $args[4]
+$appDataDir = $args[5]
+$uid        = $args[6]
 
 echo $appName $appVerPrev $appVerNext $appPort $appDataDir $uid
 
@@ -32,13 +34,14 @@ docker run --restart always -d `
  --hostname=dp-${appName}-${appVerNext}-${appPort} `
  --network=dp-net `
  -p ${appPort}:8081 `
+ -p ${appPortUi}:8082 `
  -e TZ='Europe/Moscow' `
  -v /host_mnt/c/assets/app/${appDataDir}:/var/opt/jfrog/artifactory `
  docker.bintray.io/jfrog/artifactory-oss:${appVerNext}
 @(docker ps -aqf "name=${appName}_${appVerPrev}_${appPort}" --filter status=exited) | %{docker rm -v -f $(docker ps -aqf "name=${appName}_${appVerPrev}_${appPort}" --filter status=exited)}
 @(docker images -f dangling=true -q) | %{docker rmi $(docker images -f dangling=true -q)}
 
-if [ ! -z ${uid} ]; then
+if (${uid}) {
   # Docker push
   docker login
 
@@ -49,6 +52,6 @@ if [ ! -z ${uid} ]; then
   docker push ${uid}/jfrog-artifactory-oss
 
   docker logout
-fi
+}
 
 echo Success
